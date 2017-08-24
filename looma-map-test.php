502a22c30605c25af9c48981bc4ab78bdb40fa55
skimenlo <skip@stritter.com>
2017-08-24 15:48:25 -0700

    preliminary drop for Looma 3.0 Aug 24 2017 Skip

---
+++ looma-map-test.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<!--
+Filename: looma-maps-asianCapitals.php
+Date: 2017 07
+Description:
+
+Author: Julia, Mohini, Matt, Matthew
+Owner:  VillageTech Solutions (villagetechsolutions.org)
+Looma version 3.0
+File: header.php
+-->
+
+<html>
+<?php $page_title = 'Asian Capitals Map';
+include ('includes/header.php');
+?>
+
+<link rel="stylesheet" href="css/looma-map.css">
+<link rel="stylesheet" href="css/leaflet.css">
+<link rel="stylesheet" href="css/looma-maps-asianCapitals.css" />
+
+<body>
+<h2>Asian Capitals</h2>
+<div id="main-container-horizontal">
+    <div class="viewer" id="fullscreen">
+        <button id="fullscreen-control"></button>
+        <div id="map" style="width: 100%; height: 100%"></div>
+
+    </div>
+</div>
+<?php include ('includes/toolbar.php'); ?>
+<?php include ('includes/js-includes.php'); ?>
+<!--Include other JS here -->
+<script src="js/leaflet.js"></script>
+<script src="js/topojson.js"></script>
+<script src="js/looma-screenfull.js"></script>
+<script src="js/looma-map-test.js"></script>
+
+</body>
+</html>
